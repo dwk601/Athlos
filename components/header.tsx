deaf0857be59a4a976586db013d55976a66b7221
dwk601 <dkim@uvu.edu>
2024-09-12 16:37:17 -0600

    Added sign in / sign up with kinde auth

--- components/header.tsx
+++ components/header.tsx
@@ -1,20 +1,26 @@
-import Link from 'next/link'
-import { Button } from './ui/button'
+import Link from "next/link";
+import { Button } from "./ui/button";
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Header() {
   return (
     <header className="bg-primary text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold">Athlos</Link>
+        <Link href="/" className="text-2xl font-bold">
+          Athlos
+        </Link>
         <nav>
           <Button variant="ghost" asChild>
-            <Link href="/login">Login</Link>
+            <LoginLink>Sign in</LoginLink>
           </Button>
           <Button variant="outline" asChild>
-            <Link href="/signup">Sign Up</Link>
+            <RegisterLink>Sign up</RegisterLink>
           </Button>
         </nav>
       </div>
     </header>
-  )
+  );
 }
