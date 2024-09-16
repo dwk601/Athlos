260c95fd377d7b8d94575235cad50e25e0dc2b69
Dongook <10829539@uvu.edu>
2024-09-16 10:50:31 -0600

    Delete client.ts
    duplicate supabase clients

--- utils/supabase/client.ts
+++
@@ -1,7 +0,0 @@
-// utils/supabase/client.ts
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://fxijwmoevjlzewncarjc.supabase.co';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
